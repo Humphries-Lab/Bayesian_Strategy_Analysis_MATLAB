7901e9d556fe82b54c308cb054d9f21d19477548
mdhumphries <drmdhumphries+git@gmail.com>
2022-04-20 14:19:05 +0100

    fixed cue-based strategies

--- Strategy_models/lose_shift_cued.m
+++ Strategy_models/lose_shift_cued.m
@@ -1,25 +1,39 @@
 function trial_type = lose_shift_cued(trial_data)
 
-% LOSE_SHIFT_CUED checks if subject shifted from cued option again after no
-% reward from choosing cued option
+% LOSE_SHIFT_CUED checks if subject shifted choice based on the cue, after no
+% reward
 % TYPE = LOSE_SHIFT_CUED(TRIAL_DATA) takes the Table of data TRIAL_DATA up to the current trial, and
 % returns the TYPE ('success','failure','null')
 %
-% Mark Humphries 31/3/2022
+% NOTE: this is lose-shift based on the cue, so is either:
+% (a) chose cued option when unrewarded, then chose uncued option on next trial
+% (b) chose uncued option when unrewarded, then chose cued option on next
+% trial
+% 
+% 31/3/2022 Initial version
+% 20/4/2022 Add complete set of conditions
+% Mark Humphries 
 
 % default is that trial did not meet criteria for win-stay 
 trial_type = "null";
 number_trials = size(trial_data,1);
 
-% if more than 1 trial AND chose cued option AND was NOT rewarded on the previous trial then
+% if more than 1 trial AND was NOT rewarded on the previous trial then is
 % candidate for lose-shift
-if number_trials > 1 && trial_data.Choice(end-1) == trial_data.CuePosition(end-1) && ...
-        trial_data.Reward(end-1) == "no"
-    if trial_data.Choice(end) ~= trial_data.CuePosition(end)       
-        % and did NOT choose the cued option this time
+if number_trials > 1 && trial_data.Reward(end-1) == "no"
+    
+    if trial_data.Choice(end-1) == trial_data.CuePosition(end-1) && ...
+            trial_data.Choice(end) ~= trial_data.CuePosition(end)       
+        % if chose cued option on previous trial AND chose an 
+        % uncued option on this trial
+        trial_type = "success";
+    elseif trial_data.Choice(end-1) ~= trial_data.CuePosition(end-1) && ...
+            trial_data.Choice(end) == trial_data.CuePosition(end)       
+        % if chose an uncued option on the previous trial AND did choose the
+        % cued option on this trial
         trial_type = "success";
     else
-        % chose the cued option again this time
+        % chose the same cue-based option on the current trial
         trial_type = "failure"; 
     end
 end
--- Strategy_models/win_stay_cued.m
+++ Strategy_models/win_stay_cued.m
@@ -5,20 +5,27 @@ function trial_type = win_stay_cued(trial_data)
 % TYPE = WIN_STAY_CUED(TRIAL_DATA) takes the Table of data TRIAL_DATA up to the current trial, and
 % returns the TYPE ('success','failure','null')
 %
-% Mark Humphries 31/3/2022
+% 31/3/2022 Initial version
+% 20/4/2022 Add complete set of conditions
+% Mark Humphries 
 
 % default is that trial did not meet criteria for win-stay 
 trial_type = "null";
 number_trials = size(trial_data,1);
 
-% if more than one trial, AND chose cued option AND was rewarded on the previous trial then candidate for win-stay
-if number_trials > 1 && trial_data.Choice(end-1) == trial_data.CuePosition(end-1) && ... 
-        trial_data.Reward(end-1) == "yes"
-    if trial_data.Choice(end) == trial_data.CuePosition(end)       
-        % and chose cued option this time
+% if more than one trial AND was rewarded on the previous trial then candidate for win-stay
+if number_trials > 1 && trial_data.Reward(end-1) == "yes"
+    if trial_data.Choice(end-1) == trial_data.CuePosition(end-1) && trial_data.Choice(end) == trial_data.CuePosition(end)       
+        % if chose the cued option on previous trial AND chose an
+        % cued option on this trial too
         trial_type = "success";
+    elseif trial_data.Choice(end-1) ~= trial_data.CuePosition(end-1) && ...
+            trial_data.Choice(end) ~= trial_data.CuePosition(end)       
+        % if did NOT choose the cued option on the previous trial AND chose an
+        % uncued option on this trial too
+        trial_type = "success";    
     else
-        % chose the uncued option this time
+        % switched option based on cue
         trial_type = "failure"; 
     end
 end
