37b454ea41240eacf91f6a1bcdb1d22d67bbbf89
mdhumphries <drmdhumphries+git@gmail.com>
2022-06-09 16:49:35 +0100

    Create template_for_strategy_models.m

---
+++ Strategy_models/template_for_strategy_models.m
@@ -0,0 +1,7 @@
+function trial_type = MODEL_NAME(trial_data)
+
+% Template file for a strategy model function
+% 	Input: Table of data TRIAL_DATA up to the current trial; each strategy model function assigns a type to the current trial only.
+%	Return: the TRIAL_TYPE of the trial according to the model ('success','failure','null') 
+
+
